54d3af01aadfe52c784b9879abc75474c1cd26c9
MilkyCode13 <shmayhel.andrey@gmail.com>
2024-11-04 18:10:06 +0300

    fix export

--- internal/bot/ml/export.go
+++ internal/bot/ml/export.go
@@ -2,18 +2,46 @@ package ml
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 
 	"github.com/ShmaykhelDuo/battler/internal/bot/minimax"
 )
 
-func ExportDataset(out io.Writer, data []minimax.Entry, format Format) error {
-	res := make([]map[string]Tensorable, len(data))
+var (
+	start = []byte("[")
+	sep   = []byte(",")
+	end   = []byte("]")
+)
 
-	for i, e := range data {
-		res[i] = format.Row(e.State)
-		res[i]["result"] = TensorableValue[int64]{int64(e.Result[e.State.TurnState][0])}
+func ExportDataset(out io.Writer, data []minimax.Entry, format Format) error {
+	_, err := out.Write(start)
+	if err != nil {
+		return fmt.Errorf("write start: %w", err)
 	}
 
-	return json.NewEncoder(out).Encode(res)
+	enc := json.NewEncoder(out)
+
+	for i, e := range data {
+		if i != 0 {
+			_, err := out.Write(sep)
+			if err != nil {
+				return fmt.Errorf("write sep: %w", err)
+			}
+		}
+
+		res := format.Row(e.State)
+		res["result"] = TensorableValue[int64]{int64(e.Result[e.State.TurnState][0])}
+		err := enc.Encode(res)
+		if err != nil {
+			return fmt.Errorf("encode: %w", err)
+		}
+	}
+
+	_, err = out.Write(end)
+	if err != nil {
+		return fmt.Errorf("write end: %w", err)
+	}
+
+	return nil
 }
